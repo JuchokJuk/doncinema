f7c8ba5a3bc7c73c1e30a3561485d190cd2c9286
juchokJuk <juk.juchok@yandex.ru>
2022-01-08 01:55:58 +0300

    dark theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SwiperModule } from 'swiper/angular';
-import { MenuComponent } from './ui/menu/menu.component';
-import { HeaderComponent } from './ui/header/header.component';
+import { MenuComponent } from './ui/root-components/menu/menu.component';
+import { HeaderComponent } from './ui/root-components/header/header.component';
 import { BlockComponent } from './ui/block/block.component';
 import { OrbsComponent } from './ui/orbs/orbs.component';
-import { ServicesComponent } from './ui/services/services.component';
+import { ServicesComponent } from './ui/root-components/services/services.component';
 import { HeadlineComponent } from './ui/headline/headline.component';
 import { TabsComponent } from './ui/tabs/tabs.component';
 import { SliderComponent } from './ui/slider/slider.component';
 import { DiagonalComponent } from './ui/diagonal/diagonal.component';
-import { ProjectsComponent } from './ui/projects/projects.component';
+import { ProjectsComponent } from './ui/root-components/projects/projects.component';
 import { ImageComponent } from './ui/image/image.component';
-import { PartnersComponent } from './ui/partners/partners.component';
-import { ContactsComponent } from './ui/contacts/contacts.component';
+import { PartnersComponent } from './ui/root-components/partners/partners.component';
+import { ContactsComponent } from './ui/root-components/contacts/contacts.component';
 import { FooterComponent } from './ui/footer/footer.component';
 import { PartnerCardComponent } from './ui/partner-card/partner-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,6 +26,7 @@ import { PopupComponent } from './ui/popup/popup/popup.component';
 import { PostComponent } from './ui/popup/popup-content/post/post.component';
 import { CallFormComponent } from './ui/popup/popup-content/call-form/call-form.component';
 import { StarComponent } from './ui/star/star.component';
+import { ButtonComponent } from './ui/button/button.component';
 
 @NgModule({
   declarations: [
@@ -51,6 +52,7 @@ import { StarComponent } from './ui/star/star.component';
     PostComponent,
     CallFormComponent,
     StarComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/ui/root-components/header/header.component.ts
+++ src/app/ui/root-components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ModelService, About } from 'src/app/model/model.service';
-import { CallFormComponent } from '../popup/popup-content/call-form/call-form.component';
-import { PopupService } from '../popup/popup.service';
+import { CallFormComponent } from '../../popup/popup-content/call-form/call-form.component';
+import { PopupService } from '../../popup/popup.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
