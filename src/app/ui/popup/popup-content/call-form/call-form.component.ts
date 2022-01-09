c35902792162d15ea72e0a1db900955eca4dcfe7
JuchokJuk <juk.juchok@yandex.ru>
2022-01-09 14:36:07 +0300

    button light added

--- src/app/ui/popup/popup-content/call-form/call-form.component.ts
+++ src/app/ui/popup/popup-content/call-form/call-form.component.ts
@@ -14,7 +14,7 @@ export class CallFormComponent implements OnInit {
   }
   notificate(phone: string) {
     let url = `https://api.telegram.org/bot5078699319:AAEBu-AkoyPH_YFw07ZrqWI1t2kIe1EC340/sendMessage?chat_id=-708633462&text=${phone}`;
-    fetch(url);
+    // fetch(url);
     this.popupService.closePopup();
   }
 }
